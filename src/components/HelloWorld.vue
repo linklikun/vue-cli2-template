96fe0ba6332713f4fcd81b2208cb21e3c9cfc9ec
linklikun <649014985@qq.com>
2021-02-19 15:27:26 +0800

    frist commit vue cli2  module

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <el-button type="primary">欢迎</el-button>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank"> Core Docs </a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank"> Forum </a>
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank"> Community Chat </a>
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank"> Twitter </a>
+      </li>
+      <br />
+      <li>
+        <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a href="http://router.vuejs.org/" target="_blank"> vue-router </a>
+      </li>
+      <li>
+        <a href="http://vuex.vuejs.org/" target="_blank"> vuex </a>
+      </li>
+      <li>
+        <a href="http://vue-loader.vuejs.org/" target="_blank"> vue-loader </a>
+      </li>
+      <li>
+        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+    <div class="btn-group" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-secondary">Left</button>
+      <button type="button" class="btn btn-secondary">Middle</button>
+      <button type="button" class="btn btn-secondary">Right</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import QWebChannel from "../assets/js/qwebchannel.js";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+    };
+  },
+  mounted() {
+     this.axios({
+                method:'post',
+                url:'api',
+                data:this.qs.stringify({    //这里是发送给后台的数据
+                      userId:this.userId,
+                      token:this.token,
+                })
+            }).then((response) =>{          //这里使用了ES6的语法
+                console.log(response)       //请求成功返回的数据
+            }).catch((error) =>{
+              console.log(error)  
+            })
+  }
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
